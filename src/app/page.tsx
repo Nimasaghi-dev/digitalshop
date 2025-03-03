b1c6d7fa381fae5c423a0b7454f04f691f65e9b3
nima <nimasaghi.dev@gmail.com>
2025-03-03 22:59:49 +0100

    feat: add carousel-images-button-welcome-banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
+import Banner from "@/components/banner";
+import { Button } from "@/components/ui/button";
+import Welcome from "@/components/Welcome";
+
 export default function Home() {
-  // console.log('Test');
-
-  const renderName = (name2: any) => {
-    const name = 'nima';
-    return <span>{name}</span>;
-  };
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>My first page</div>
-      {renderName(1)}
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Banner />
+      <Welcome />
     </div>
   );
 }
---
+++ src/components/banner/index.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+    Card, CardContent
+} from "@/components/ui"
+
+import IMG1 from "./image/1-daniel-korpai-KPCPE_sNBdQ-unsplash.jpg";
+import IMG2 from "./image/2-carl-heyerdahl-KE0nC8-58MQ-unsplash.jpg";
+import IMG3 from "./image/3-dennis-brendel-gQ6NlDvRngU-unsplash.jpg";
+import IMG4 from "./image/4-ab-wgslH4XlNCc-unsplash.jpg";
+import Image from "next/image";
+
+function Banner() {
+    return (
+        <Carousel className="w-full">
+            <CarouselContent>
+                {[IMG1, IMG2, IMG3, IMG4].map((image, index) => (
+                    <CarouselItem key={index}>
+                        <div className="p-1">
+                            <Card>
+                                <CardContent className="relative aspect-square h-[400px] w-full flex items-center justify-center p-6">
+                                    <Image src={image} alt="banner" fill className="object-cover"/>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    )
+}
+
+export default Banner;
