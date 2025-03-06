392d31b3d5950fa4fc690ba5bd71efe784f97833
nima <nimasaghi.dev@gmail.com>
2025-03-06 14:05:04 +0100

    feat: add footer

---
+++ src/app/products/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function layout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            Products layout
+        
+        {children}
+        </div>
+    )
+}
+
+export default layout;
